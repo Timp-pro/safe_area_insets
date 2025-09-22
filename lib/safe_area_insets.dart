19613f828a7403059902a5e8c3064df365f0dc57
Caeruleo <marcos.mares95@gmail.com>
2025-09-22 13:55:39 +0200

    fix: update conditional export for web library

--- lib/safe_area_insets.dart
+++ lib/safe_area_insets.dart
@@ -1,6 +1,6 @@
 library safe_area_insets;
 
 export 'src/core_stub.dart' //
-    if (dart.library.html) 'src/core.dart';
+    if (dart.library.web) 'src/core.dart';
 
 export 'src/web_safe_area_insets.dart';
